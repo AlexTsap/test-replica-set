05c6e03bdbff500d6fd8474797da20d3d263fb6e
AlexTsap <alexandr.tsap@valor-software.com>
2019-04-04 17:28:32 +0300

    chore(*): route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/test', function(req, res, next) {
+    return getArticleData((test) => {
+        res.send(test);
+        res.render('index', { title: 'Express' });
+    });
 });
+
+const articles = mongoose.model('Articles');
+
+async function getArticleData(cb) {
+    return articles
+        .findOne()
+        .lean()
+        .exec(cb);
+}
 
 module.exports = router;
