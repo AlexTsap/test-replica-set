edf070728e1f4239e1c16c68a5c65184e783c0b5
AlexTsap <alexandr.tsap@valor-software.com>
2019-04-05 16:41:42 +0300

    fix(server): change query

--- routes/test.js
+++ routes/test.js
@@ -2,27 +2,13 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
-const articles = mongoose.model('Articles');
 const places = mongoose.model('Places');
 
 router.get('/', async function(req, res, next) {
     const placesIds = await getPlacesId();
-    const thingArr = [];
-    for (const place of placesIds) {
-        const result = await getArticleData(place);
-        console.log(result);
 
-        thingArr.push(result);
-    }
-
-    return res.render('success', {title: thingArr});
+    return res.render('success', {title: placesIds});
 });
-
-function getArticleData(place) {
-    return articles
-        .find({_id: place._id}, {thing: 1})
-        .exec();
-}
 
 function getPlacesId() {
     return places
