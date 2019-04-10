8dfa404d50a26f2194eb004582de34ae1ccd9c15
AlexTsap <alexandr.tsap@valor-software.com>
2019-04-10 18:55:19 +0300

    chore(*): use while correctly

--- routes/test.js
+++ routes/test.js
@@ -6,71 +6,45 @@ const mongoose = require('mongoose');
 const places = mongoose.model('Places');
 const infoPlaces = mongoose.model('InfoPlaces');
 
-const asyncMiddleware = fn =>
-    (req, res, next) => {
-        Promise.resolve(fn(req, res, next))
-            .catch(next);
-    };
-
-router.get('/', asyncMiddleware(async function (req, res, next) {
-    try {
-        const placesIds = await getPlacesId();
-        const info = await getInfoPlaceByPlacesIds(placesIds);
-
-        return res.send({success: true, body: info});
-    } catch (e) {
-        return res.send({success: false, body: e});
-    }
-}));
+// const asyncMiddleware = fn =>
+//     (req, res, next) => {
+//         Promise.resolve(fn(req, res, next))
+//             .catch(next);
+//     };
+//
+// router.get('/', asyncMiddleware(async function (req, res, next) {
+//     try {
+//         const placesIds = await getPlacesId();
+//         const info = await getInfoPlaceByPlacesIds(placesIds);
+//
+//         return res.send({success: true, body: info});
+//     } catch (e) {
+//         return res.send({success: false, body: e});
+//     }
+// }));
 
 router.get('/', async (req, res, next) => {
-    const placesIds = await getPlacesId();
-    try {
-        const info = await getInfoPlaceByPlacesIds(placesIds);
+    let counter = 3;
+    let placesIds;
+    let info;
+    let error = true;
 
-        return res.send({success: true, body: info});
-    } catch (e) {
-        let t = 0;
-        let info;
-        let breakTheLoop = false;
-        while(t < 2 && !breakTheLoop){
-            const placesIds = await getPlacesId();
+    while (counter && error) {
+        try {
+            placesIds = await getPlacesId();
             info = await getInfoPlaceByPlacesIds(placesIds);
-
-            if(info) {
-                breakTheLoop = true;
-                return res.send({success: true, body: info});
-            }
+        } catch (e) {
+            counter--;
+            error = e;
         }
-
-        return res.send({success: false, body: e});
     }
-});
 
-// router.get('/', async function (req, res, next) {
-//     const placesIds = await getPlacesId();
-//     const info = await getInfoPlaceByPlacesIds(placesIds);
-//
-//     if (!placesIds) {
-//         async.retry(5, await getPlacesId, async function(err, placesIds) {
-//             const info = await getInfoPlaceByPlacesIds(placesIds);
-//
-//             if (!info) {
-//                 async.retry(5, await getInfoPlaceByPlacesIds, async function(err, info) {
-//                     return res.send({success: true, body: info});
-//                 });
-//             }
-//         });
-//     }
-//
-//     if (!info) {
-//         async.retry(5, await getInfoPlaceByPlacesIds, async function(err, info) {
-//             return res.send({success: true, body: info});
-//         });
-//     }
-//
-// return res.send({success: true, body: info});
-// });
+    if (info) {
+        return res.send({success: true, body: info});
+    }
+
+    return next(error);
+});
 
 async function getPlacesId() {
     return places
